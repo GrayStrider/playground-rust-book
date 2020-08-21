e1e3c1946b9e0c54457853b768a4959a3150e7e4
Gray Strider <zhoga.ivan@gmail.com>
2020-08-21 19:18:24 +1100

    comments

---
+++ src/comments.rs
@@ -0,0 +1,19 @@
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let five = 5;
+///
+/// assert_eq!(6, add_one(5));
+/// # fn add_one(x: i32) -> i32 {
+/// #     x + 1
+/// # }
+/// ```
+fn add_one(x: i32) -> i32 {
+	x + 1
+}
+
+/// You can use the rustdoc tool to generate
+/// HTML documentation from these doc comments,
+/// and also to run the code examples as tests!
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! # The Rust Standard Library
+//!
+//! The Rust Standard Library provides the essential runtime
+//! functionality for building portable Rust software.
+
 mod bindings;
 mod funcs;
 mod primitives;
+mod comments;
