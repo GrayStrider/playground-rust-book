422be68afb5fce2a9a5792b702b4a02e3d57b449
Gray Strider <zhoga.ivan@gmail.com>
2020-08-24 19:37:23 +1100

    rem: old

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,1 @@
-fn main() {
-	let _x = 5;
-	
-	let controller: Controller = |data| {
-		let data = data.parse()
-			.expect("Bad string");
-		Res { data, meta: 10 }
-	};
-}
-
-type Controller = fn (data: str) -> Res;
-
-struct Res {
-	data: String,
-	meta: i32
-}
+fn main() {}
